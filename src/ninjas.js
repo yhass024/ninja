d82861fb193bad42abfe2c2284f3cf2ba0c98618
Yusuf <yusuf@Yusufs-MacBook-Pro.local>
2020-06-30 23:31:31 -0400

    initial commit

---
+++ src/ninjas.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Ninjas = ({ ninjas, deleteNinja }) => {
+  const ninjaList = ninjas.map((ninja) => {
+    if (ninja.age > 20) {
+      return (
+        <div className="ninja" key={ninja.id}>
+          <div> Name: {ninja.name}</div>
+          <div> Age: {ninja.age}</div>
+          <div> Belt: {ninja.belt}</div>
+          <button
+            onClick={() => {
+              deleteNinja(ninja.id);
+            }}
+          >
+            Delete
+          </button>
+          <hr></hr>
+        </div>
+      );
+    } else return null;
+  });
+  return <div className="ninja-list">{ninjaList}</div>;
+};
+
+export default Ninjas;
