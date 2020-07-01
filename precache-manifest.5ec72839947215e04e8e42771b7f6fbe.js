2a0fc4ad5442b8ce6d2eb501a72e7e4c97cbfda6
Yusuf <yusuf@Yusufs-MacBook-Pro.local>
2020-07-01 00:08:09 -0400

    Updates

---
+++ precache-manifest.5ec72839947215e04e8e42771b7f6fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64faff8854d2a4d27af67616caa7fa9c",
+    "url": "/ninja/index.html"
+  },
+  {
+    "revision": "d6cdcf027538bf268836",
+    "url": "/ninja/static/css/main.cd4bebc1.chunk.css"
+  },
+  {
+    "revision": "137db4ce08f10d746302",
+    "url": "/ninja/static/js/2.55875d4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ninja/static/js/2.55875d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6cdcf027538bf268836",
+    "url": "/ninja/static/js/main.b26686d0.chunk.js"
+  },
+  {
+    "revision": "32aeaa90457daba0c8ba",
+    "url": "/ninja/static/js/runtime-main.dbec87c4.js"
+  }
+]);
